a3206ee6f9728b164b5f0b68210f93dada1aff4a
erick-bonfati <erickbonfati39@gmail.com>
2026-05-28 20:14:11 -0300

    SegurançaWebInicio

---
+++ SegurancaWeb/public/index.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+use Alura\Mvc\Controller\{
+    Controller,
+    DeleteVideoController,
+    EditVideoController,
+    Error404Controller,
+    NewVideoController,
+    VideoFormController,
+    VideoListController
+};
+use Alura\Mvc\Repository\VideoRepository;
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$pdo = new PDO('mysql:host=localhost;dbname=aluratube', 'root', 'admin');
+
+$videoRepository = new VideoRepository($pdo);
+
+$routes = require_once __DIR__ . '/../config/routes.php';
+
+$pathInfo = $_SERVER['PATH_INFO'] ?? '/';
+$httpMethod = $_SERVER['REQUEST_METHOD'];
+
+$key = "$httpMethod|$pathInfo";
+if (array_key_exists($key, $routes)) {
+    $controllerClass = $routes["$httpMethod|$pathInfo"];
+
+    $controller = new $controllerClass($videoRepository);
+} else {
+    $controller = new Error404Controller();
+}
+/** @var Controller $controller */
+$controller->processaRequisicao();
